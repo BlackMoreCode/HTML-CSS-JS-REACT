fb7dab651567c1e4f57e363ad988be70abf726a7
BlackMoreCode <sehorim1993@gmail.com>
2024-12-30 14:57:59 +0900

    2024-12-30

--- React/kh-mini-project-241126/src/api/AxiosApi.js
+++ React/kh-mini-project-241126/src/api/AxiosApi.js
@@ -1,8 +1,10 @@
 import axios from "axios";
+import AxiosInstance from "./AxiosInstance";
+
 const KH_DOMAIN = "http://localhost:8111";
 
 const AxiosApi = {
-  // 로그인 수정
+  // 로그인
   login: async (email, pw) => {
     console.log("이메일 : ", email);
     console.log("패스워드 : ", pw);
@@ -17,13 +19,13 @@ const AxiosApi = {
     return await axios.get(KH_DOMAIN + `/auth/exists/${email}`);
   },
   // 회원 가입
-  signup: async (email, pw, name) => {
+  signup: async (email, pwd, name) => {
     console.log("이메일 : ", email);
-    console.log("패스워드 : ", pw);
+    console.log("패스워드 : ", pwd);
     console.log("이름 : ", name);
     const member = {
       email: email,
-      pwd: pw,
+      pwd: pwd,
       name: name,
       imgPath: "",
     };
@@ -31,13 +33,14 @@ const AxiosApi = {
   },
   // 전체 회원 조회
   memberList: async () => {
-    return await axios.get(KH_DOMAIN + "/member/list");
+    // return await axios.get(KH_DOMAIN + "/member/list");
+    return await AxiosInstance.get(`/member/list`);
   },
   // 개별 회원 조회
   memberInfo: async (email) => {
-    return await axios.get(KH_DOMAIN + `/member/${email}`);
+    return await AxiosInstance.get(`/member/${email}`);
   },
-  // 채팅방 목록 가져 오기
+  // 채팅방 목록 가져오기
   chatList: async () => {
     return await axios.get(KH_DOMAIN + `/chat/list`);
   },
@@ -50,7 +53,7 @@ const AxiosApi = {
     };
     return await axios.post(KH_DOMAIN + `/chat/new`, chat);
   },
-  // 채팅방 정보 가져오기
+  // 채팅방 정보 가져 오기
   chatDetail: async (roomId) => {
     return await axios.get(KH_DOMAIN + `/chat/room/${roomId}`);
   },
